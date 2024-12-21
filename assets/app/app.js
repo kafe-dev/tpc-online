46299615d45d6f1ff1d4ba66467d5eaa20c18d9f
giang <giang8692@gmail.com>
2024-12-21 12:20:11 +0700

    Add: CSS and JS dependency

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,5 +1,31 @@
-import '../bootstrap.js';
+// Import CSS
+import './css/font-awesome.min.css';
+import './css/animate.css';
+import './css/themify-icons.css';
+import './css/stroke-gap.css';
+import './css/material-design-iconic-font.min.css';
+import './css/nice-select.css';
+import './css/jquery.fancybox.css';
+import './css/jquery-ui.min.css';
+import './css/meanmenu.min.css';
+import './css/owl.carousel.min.css';
+import './css/slick.css';
+import './css/bootstrap.min.css';
+import './css/default.css';
+import './css/style.css';
+import './css/responsive.css';
 
-// Stylesheets
+// Import JavaScript
+import './js/vendor/modernizr-3.5.0.min.js';
+import './js/vendor/jquery-3.3.1.min.js';
+import 'bootstrap';
+import './js/jquery.meanmenu.min.js';
+import './js/jquery.scrollUp.js';
+import './js/jquery.fancybox.min.js';
+import './js/jquery.countdown.min.js';
+import './js/jquery.nice-select.min.js';
+import './js/owl.carousel.min.js';
+import './js/slick.min.js';
+import './js/plugins.js';
+import './js/main.js';
 
-// Javascripts
