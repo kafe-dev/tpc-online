e3c6356ab921af9f1d1bccacd65f868d490186be
giang <giang8692@gmail.com>
2024-12-22 13:17:11 +0700

    Add : Add UI for 404, Forgot Password, Login, Register, About Us, Blog, Blog Details Page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -120,4 +120,95 @@ class HomeController extends AbstractController
 
         return $this->render('home/contact_us.html.twig', ['title' => $title]);
     }
+
+    /**
+     * Action About Us.
+     *
+     * This is About Us page controller
+     */
+    #[Route('/about-us', name: 'about_us')]
+    public function about(): Response
+    {
+        $title = "About Us";
+
+        return $this->render('home/about_us.html.twig', ['title' => $title]);
+    }
+
+    /**
+     * Action Register.
+     *
+     * This is Register page controller
+     */
+    #[Route('/register', name: 'register')]
+    public function register(): Response
+    {
+        $title = "Register";
+
+        return $this->render('home/register.html.twig', ['title' => $title]);
+    }
+
+    /**
+     * Action Login.
+     *
+     * This is Login page controller
+     */
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        $title = "Login";
+
+        return $this->render('home/login.html.twig', ['title' => $title]);
+    }
+
+    /**
+     * Action 404.
+     *
+     * This is 404-page controller
+     */
+    #[Route('/404', name: '404')]
+    public function page404(): Response
+    {
+        $title = "Page Not Found";
+
+        return $this->render('home/404.html.twig', ['title' => $title]);
+    }
+
+    /**
+     * Action forgot password.
+     *
+     * This is Forgot password page controller
+     */
+    #[Route('/forgot-password', name: 'forgot_password')]
+    public function forgotPassword(): Response
+    {
+        $title = "Forgot Password";
+
+        return $this->render('home/forgot_password.html.twig', ['title' => $title]);
+    }
+
+    /**
+     * Action blog.
+     *
+     * This is Blog page controller
+     */
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        $title = "Blog";
+
+        return $this->render('home/blog.html.twig', ['title' => $title]);
+    }
+
+    /**
+     * Action blog details.
+     *
+     * This is Blog Details page controller
+     */
+    #[Route('/blog-details', name: 'blog_details')]
+    public function blogDetails(): Response
+    {
+        $title = "Blog Details";
+
+        return $this->render('home/blog_details.html.twig', ['title' => $title]);
+    }
 }
