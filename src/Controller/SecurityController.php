9313cda4f9bdb136c51fe548bd063c367646b190
giang <giang8692@gmail.com>
2024-12-24 00:51:10 +0700

    Update: Add login and logout route

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Class HomeController.
@@ -23,8 +25,30 @@ class SecurityController extends AbstractController
      * and display the login page.
      */
     #[Route('/login', name: 'login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/login.html.twig');
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_dashboard_index');
+        }
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * Action login.
+     *
+     * This action is used to handle the login request
+     * and display the login page.
+     */
+    #[Route('/logout', name: 'logout')]
+    public function logout(Security $security): Response
+    {
+        return $security->logout();
     }
 }
